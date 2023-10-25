38be6c809552231c85d966dd4c6a872fd0476225
Lynxtail <l.gorbachev@mail.ru>
2023-10-25 20:53:47 +0400

    finish

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
 const double PI = 3.14159265359;
 
+/* 1 task */
+
 class Curve{
     public:
+        virtual double get_params() const = 0;
+
         virtual double get_x(double t) const = 0;
         virtual double get_y(double t) const = 0;
         virtual double get_z(double t) const = 0;
@@ -17,8 +21,7 @@ class Curve{
         virtual double get_der_y(double t) const = 0;
         virtual double get_der_z(double t) const = 0;
 
-        virtual void get_info() const = 0;
-        virtual ~Curve() {}
+        virtual void get_info(double t) const = 0;
 };
 
 class Circle : public Curve{
@@ -26,7 +29,14 @@ class Circle : public Curve{
         double radius;
     
     public:
-        Circle(double radius);
+        Circle(double _radius) {
+            if (_radius >= 0)
+                radius = _radius;
+            else
+                radius = 0; // default value
+        }
+
+        double get_params() const override { return radius; }
 
         double get_x(double t) const override {
             return radius * cos(t);
@@ -52,15 +62,16 @@ class Circle : public Curve{
             return 0.;
         }
 
-        void get_info() const override {
+        void get_info(double t) const override {
             cout << "Circle:\n";
-            cout << "x = " << get_x(PI / 4) << ", ";
-            cout << "y = " << get_y(PI / 4) << ", ";
-            cout << "z = " << get_z(PI / 4) << "\n";
-            cout << "dx / dt = " << get_der_x(PI / 4) << ", ";
-            cout << "dy / dt = " << get_der_y(PI / 4) << ", ";
-            cout << "dz / dt = " << get_der_z(PI / 4) << "\n";
-
+            cout << "Point at t = " << t << " {";
+            cout << get_x(t) << ", ";
+            cout << get_y(t) << ", ";
+            cout << get_z(t) << "}\n";
+            cout << "3D vector at t = " << t << " {";
+            cout << get_der_x(t) << ", ";
+            cout << get_der_y(t) << ", ";
+            cout << get_der_z(t) << "}\n";
         }
 };
 
@@ -70,7 +81,16 @@ class Ellipse : public Curve{
         double radius_y;
     
     public:
-        Ellipse(double radius_x, double radius_y);
+        Ellipse(double _radius_x, double _radius_y) {
+            if (_radius_x >= 0 || _radius_y >= 0) {
+                radius_x = _radius_x; radius_y = _radius_y;
+            }
+            else {
+                radius_x = 0; radius_y = 0; // default value
+            }
+        }
+
+        double get_params() const override { return radius_x, radius_y; }
 
         double get_x(double t) const override {
             return radius_x * cos(t);
@@ -96,15 +116,16 @@ class Ellipse : public Curve{
             return 0.;
         }
 
-        void get_info() const override {
+        void get_info(double t) const override {
             cout << "Ellipse:\n";
-            cout << "x = " << get_x(PI / 4) << ", ";
-            cout << "y = " << get_y(PI / 4) << ", ";
-            cout << "z = " << get_z(PI / 4) << "\n";
-            cout << "dx / dt = " << get_der_x(PI / 4) << ", ";
-            cout << "dy / dt = " << get_der_y(PI / 4) << ", ";
-            cout << "dz / dt = " << get_der_z(PI / 4) << "\n";
-
+            cout << "Point at t = " << t << " {";
+            cout << get_x(t) << ", ";
+            cout << get_y(t) << ", ";
+            cout << get_z(t) << "}\n";
+            cout << "3D vector at t = " << t << " {";
+            cout << get_der_x(t) << ", ";
+            cout << get_der_y(t) << ", ";
+            cout << get_der_z(t) << "}\n";
         }
 };
 
@@ -114,7 +135,15 @@ class Helix : public Curve{
         double step;
     
     public:
-        Helix(double radius, double step);
+        Helix(double _radius, double _step) {
+            if (_radius >= 0)
+                radius = _radius;
+            else
+                radius = 0; // default value
+            step = _step;
+        }
+
+        double get_params() const override { return radius, step; }
 
         double get_x(double t) const override {
             return (radius + step * t) * cos(t);
@@ -140,19 +169,81 @@ class Helix : public Curve{
             return 1.;
         }
 
-        void get_info() const override {
+        void get_info(double t) const override {
             cout << "Helix:\n";
-            cout << "x = " << get_x(PI / 4) << ", ";
-            cout << "y = " << get_y(PI / 4) << ", ";
-            cout << "z = " << get_z(PI / 4) << "\n";
-            cout << "dx / dt = " << get_der_x(PI / 4) << ", ";
-            cout << "dy / dt = " << get_der_y(PI / 4) << ", ";
-            cout << "dz / dt = " << get_der_z(PI / 4) << "\n";
-
+            cout << "Point at t = " << t << " {";
+            cout << get_x(t) << ", ";
+            cout << get_y(t) << ", ";
+            cout << get_z(t) << "}\n";
+            cout << "3D vector at t = " << t << " {";
+            cout << get_der_x(t) << ", ";
+            cout << get_der_y(t) << ", ";
+            cout << get_der_z(t) << "}\n";
         }
 };
 
+bool compare(Curve* a, Curve* b){
+    return a->get_params() < b->get_params();
+}
+
 int main(){
-    cout << "Hey!\n";
+
+    /* 2 task */
+    vector<Curve*> first_v;
+    int cur_type; 
+    srand(time(NULL));
+    for (int i = 0; i < 5; i++){
+        cur_type = rand() % 3;
+        switch (cur_type)
+        {
+        case 0:
+            // circle
+        {
+            double radius = rand() % 15 + 1; // [1, 15]
+            first_v.push_back(new Circle(radius));
+            break;
+        }
+        case 1:
+            // ellipse
+        {
+            double radius_x = rand() % 15 + 1; // [1, 15]
+            double radius_y = rand() % 15 + 1; // [1, 15]
+            first_v.push_back(new Ellipse(radius_x, radius_y));
+            break;
+        }
+        default:
+            // helix
+            double radius = rand() % 15 + 1; // [1, 15]
+            double step = rand() % 10 + 1; // [1, 10]
+            first_v.push_back(new Helix(radius, step));
+            break;
+        }
+    }
+
+    /* 3 task */
+    for (Curve* item : first_v){
+        item->get_info(PI / 4);
+        cout << endl;
+    }
+
+    /* 4 task */
+    vector<Curve*> second_v;
+    for (Curve* item : first_v){
+        if (typeid(*item) == typeid(Circle)) { 
+            second_v.push_back(item);        
+        }
+
+    }
+
+    /* 5 task */
+    sort(second_v.begin(), second_v.end(), compare);
+
+    /* 6 task */
+    double sum = 0;
+    for (Curve* item : second_v){
+        sum += item->get_params();
+    }
+    cout << "Total sum of radii of all cirles: " << sum << endl;
+
     return 0;
 }
